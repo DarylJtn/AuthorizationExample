e7437c3309e346f816f09ca8d9d000953acfec7e
Daryl Johnston <daryl.johnston@sentel.co.uk>
2022-08-07 00:55:23 +0100

    added missing logic to get user input from console

--- NantHealthAuthorize/Program.cs
+++ NantHealthAuthorize/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NantHealthAuthorize;
+using NantHealthAuthorize.Models;
+while (true) { 
+    Console.WriteLine("Enter Username:");
+    string username = Console.ReadLine();
+    Console.WriteLine("Enter Password:");
+    string password = Console.ReadLine();
+    DBLogic logic = new DBLogic();
+    User user = logic.GetUser(username);
+    if (user != null && LoginLogic.ValidCredientials(user, password))
+    {
+        Console.WriteLine("Login Successful");
+    }
+    else {
+        Console.WriteLine("Login Unsuccessful");
+    }
+}
