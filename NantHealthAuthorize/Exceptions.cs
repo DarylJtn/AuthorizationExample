bc3316dfae7996fdf0d90dabc092a3ffcfb94655
Daryl Johnston <daryl.johnston@sentel.co.uk>
2022-08-07 00:39:50 +0100

    removed gitignore files

---
+++ NantHealthAuthorize/Exceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NantHealthAuthorize
+{
+    public class Missing : Exception
+    {
+    }
+}
